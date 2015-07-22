1496d3450e39e5b191ce1fe671e44c8713e199f7
leonetti <leonetti.tech@gmail.com>
2015-07-22 15:52:28 -0700

    one test pass

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -6,16 +6,46 @@ exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  "access-control-max-age": 10//, // Seconds.
+  //'Content-Type': "text/html"
 };
+
+exports.validExtensions = validExtensions = {
+  'html' : 'text/html',
+  'css' : 'text/css',
+  'txt' : 'text/plain',
+  'jpg' : 'img/jpeg',
+  'gif' : 'img/gif',
+  'png' : 'img/png',
+  'js' : 'application/javascript',
+  'ico': 'image/x-icon'
+};
+
+exports.getHeader = function(contentType){
+  var header = headers;
+  header['Content-Type'] = validExtensions[contentType];
+
+  return header;
+}
 
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  callback = callback || function(x) { return x; };
+  var assetPath = path.join(__dirname, "public", asset);
+  fs.readFile(assetPath, function(err, data){
+    if (err){
+      throw err;
+    }
+    res.write(callback(data));
+    res.end();
+    //callback(data);
+  });
+
+  //serve index.html
+  //serve loading.html
+  //serve styles.css
 };
-
-
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,67 @@
 var path = require('path');
+var http = require('http');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+
+var actions = {
+  'GET': function(request, response){
+
+    var reqURL = url.parse(request.url).href;
+    console.log("-----> reqURL: ", reqURL);
+    // check if the file is archived
+    var checkURL = archive.paths.archivedSites + "/" + reqURL;
+
+    // if not get the url
+    
+    var extension = path.extname(reqURL);
+
+    var header = httpHelpers.getHeader(extension.replace(".", ""));
+    response.writeHead(200, header);
+
+    if(reqURL === '/'){
+      httpHelpers.serveAssets(response, "index.html");
+    } else {
+      //console.log("--------> extension: " + extension);
+      //console.log("--------> basename: " + path.basename(reqURL, extension));
+      //console.log("--------> header: " + JSON.stringify(header));
+      httpHelpers.serveAssets(response, path.basename(reqURL));
+    }
+    /*utils.sendResponse(response, {results: messages});*/
+  },
+  'POST': function(request, response){
+    /*utils.collectData(request, function(message){
+      message.objectId = ++objectId;
+      messages.push(message);
+      utils.sendResponse(response, {objectId: objectId}, 201);
+    });*/
+  },
+  'OPTIONS': function(request, response){
+    /*sendResponse(response);*/
+  }
+};
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action){
+    action(req, res);
+  } else {
+    sendResponse(res, "Not Found", 404);
+    res.writeHead(404, httpHelpers.headers);
+    res.end("Not Found");
+  }
+  // res.end(archive.paths.list);
+};
+
+
+exports.sendResponse = function(res, data, statusCode){
+  statusCode = statusCode || 200;
+  res.writeHead(statusCode, httpHelpers.headers);
+  //res.write(data);
+  res.end(data);
+  //res.end(archive.paths.list);
 };
